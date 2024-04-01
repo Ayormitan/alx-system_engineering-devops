bf63ee80b45947e4dc314e699a1e4d569ebdb5ea
Ayormitan <kolawoleabaz97@gmail.com>
2024-04-01 14:05:11 -0700

    Pupprt usage

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#Using puppet to make a change in configuration file
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+content =>"
+	#SSH client configuration
+	host*
+	IdentifyFile ~/.ssh/school
+	PasswordAuthentication no
+}
