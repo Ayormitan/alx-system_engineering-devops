6b703e6a66c82b074f6ebcaf1c853ec469aec94d
Ayormitan <kolawoleabaz97@gmail.com>
2024-08-13 09:32:50 -0700

    Queries reddit subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+Script/function that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
+    """ Define the API endpoint"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """ Set the header """
     headers = {"User-Agent": "Mozilla/5.0"}
+    """ Make a GET request to Reddit API"""
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
+        """ Parse the Json response"""
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return data['data']['subscribers']
     else:
         return 0
